95cdc77880c98b6d10ba37041ff012504eef128a
dtan4 <dtanshi45@gmail.com>
2014-10-27 18:21:14 +0900

    Add IntegerPairSpec

---
+++ src/test/groovy/IntegerPairSpec.groovy
@@ -0,0 +1,22 @@
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class IntegerPairSpec extends Specification {
+    @Unroll
+    def "When a is #a and b is #b, gcd() returns #result"() {
+        setup:
+        def integerPair = new IntegerPair(a, b)
+
+        expect:
+        integerPair.gcd() == result
+
+        where:
+        a | b || result
+        2 | 3 || 1
+        12 | 18 || 6
+        12 | 12 || 12
+        12 | 0 || 12
+        0 | 0 || 0
+        12 | -18 || 6
+    }
+}
