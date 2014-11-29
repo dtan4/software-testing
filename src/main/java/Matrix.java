5cf2e14fa2ad3656c5752b64d896f8eb70349074
Daisuke Fujita <dtanshi45@gmail.com>
2014-11-29 15:00:45 +0900

    Implement arrayReader method

--- src/main/java/Matrix.java
+++ src/main/java/Matrix.java
@@ -1,4 +1,55 @@
 public class Matrix {
-  public Matrix() {
+  private Rational[][] elem;
+  private int nRow;
+  private int nCol;
+  private final Rational minusOne = new Rational(-1);
+  private final Rational zero = new Rational(0);
+
+  public Matrix(Rational[][] elem) {
+    this.elem = elem;
+    this.nRow = elem.length;
+
+    if (elem.length > 0) {
+      this.nRow = elem[0].length;
+    } else {
+      this.nRow = 0;
+    }
+  }
+
+  public Rational[][] getElem() {
+    return this.elem;
+  }
+
+  static Matrix arrayReader(long[][][] array) {
+    if ((array.length == 0) || (array[0].length == 0)) {
+      return null;
+    }
+
+    int nRow, nCol;
+
+    nRow = array.length;
+    nCol = array[0].length;
+
+    Rational[][] elem = new Rational[nRow][];
+
+    for (int i = 0; i < nRow; i++) {
+      elem[i] = new Rational[nCol];
+
+      if (array[i].length <= array[0].length) {
+        for (int j = 0; j < nCol; j++) {
+          if (j < array[i].length) {
+            elem[i][j] = Rational.arrayReader(array[i][j]);
+          } else {
+            elem[i][j] = new Rational(0);
+          }
+        }
+      } else {
+        for (int j = 0; j < nCol; j++) {
+          elem[i][j] = Rational.arrayReader(array[i][j]);
+        }
+      }
+    }
+
+    return new Matrix(elem);
   }
 }
--- src/test/java/MatrixTest.java
+++ src/test/java/MatrixTest.java
@@ -2,5 +2,33 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class MatrixTest {
+  // ********************************
+  // arrayReader()
+  // ********************************
 
+  // when:   array.length == 0
+  // expect: null
+  @Test
+  public void testArrayReader_empty() {
+    long[][][] array = {};
+    assertNull(Matrix.arrayReader(array));
+  }
+
+  // when:   array.length[0] == 0
+  // expect: null
+  @Test
+  public void testArrayReader_empty_2() {
+    long[][][] array = {{}};
+    assertNull(Matrix.arrayReader(array));
+  }
+
+  // when:   non-empty array
+  // expect: Matrix
+  @Test
+  public void testArrayReader() {
+    long[][][] array = {{{1, 2}, {3, 4}}, {{5, 6}, {7, 8}}};
+    Matrix matrix = Matrix.arrayReader(array);
+
+    assertTrue(matrix.getElem()[0][0].equals(new Rational(1, 2)));
+  }
 }
