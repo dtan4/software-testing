5440d46946a3e728d79d4af26f5fa95e79abfda6
Daisuke Fujita <dtanshi45@gmail.com>
2015-01-07 19:08:50 +0900

    Remove unused methods

--- src/main/java/LIS.java
+++ src/main/java/LIS.java
@@ -56,47 +56,6 @@ public class LIS {
         return this.x;
     }
 
-    public boolean hasValidSolution() {
-        Rational[] r = a.substVector(x);
-
-        for (int i = 0; i < aCol; i++) {
-            if (compare(r[i], b[i]) != c[i]) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    private int compare(Rational r1, Rational r2) {
-        if (r1.greaterThan(r2)) {
-            return GREATER;
-        } else if (r1.lessThan(r2)) {
-            return LESS;
-        }
-
-        return EQUAL;
-    }
-
-    public boolean hasValidNBV() {
-        int[] p = d.getP();
-
-        for (int i = aRow; i < aRow + aCol; i++) {
-            int valNum = p[i];
-
-            if ((x[valNum].lessThan(lb[valNum])) || (x[valNum].greaterThan(ub[valNum]))) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    public void init() {
-        makeHLES();
-        makeBoundaryCondition();
-    }
-
     private void makeHLES() {
         Rational[][] dElem = new Rational[aRow][aRow + aCol];
         Rational[][] aElem = a.getElem();
@@ -116,24 +75,6 @@ public class LIS {
 
         for (int i = 0; i < aRow + aCol; i++) {
             x[i] = zero;
-        }
-    }
-
-    private void makeBoundaryCondition() {
-        Rational negativeInfinity = new Rational(-Long.MAX_VALUE);
-        Rational positiveInfinity = new Rational(Long.MAX_VALUE);
-
-        lb = new Rational[aRow + aCol];
-        ub = new Rational[aRow + aCol];
-
-        for (int i = 0; i < aRow; i++) {
-            lb[i] = (c[i] == GREATER) ? b[i] : negativeInfinity;
-            ub[i] = (c[i] == LESS) ? b[i] : positiveInfinity;
-        }
-
-        for (int i = aRow; i < aRow + aCol; i++) {
-            lb[i] = negativeInfinity;
-            ub[i] = positiveInfinity;
         }
     }
 }
--- src/test/java/LISTest.java
+++ src/test/java/LISTest.java
@@ -129,36 +129,4 @@ public class LISTest {
 
         assertArrayEquals(x, lis.getX());
     }
-
-    // ********************************
-    // hasValidSolution()
-    // ********************************
-
-    // when:   correspond to c
-    // expect: true
-    @Ignore
-    @Test
-    public void testHasValidSolution_correspond() {
-        long[][][] array = {
-                {{1, 2}, {3, 4}},
-                {{5, 6}, {7, 8}}
-        };
-        Matrix a = Matrix.arrayReader(array);
-        Rational[] b = {
-                new Rational(1),
-                new Rational(2)
-        };
-        int[] c = {0, 1, 2};
-
-        LIS lis = new LIS(a, b, c);
-        assertTrue(lis.hasValidSolution());
-    }
-
-    // when:   not correspond to c
-    // expect: false
-    @Ignore
-    @Test
-    public void testHasValidSolution_notCorrespond() {
-
-    }
 }
